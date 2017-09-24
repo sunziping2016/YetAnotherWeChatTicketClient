7a7f8573cdef7665614eb249d2bbcb6638cfa9de
Sun Ziping <sunziping2016@gmail.com>
2017-09-24 22:42:32 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+if ('serviceWorker' in navigator && window.location.protocol === 'https:') {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function (registration) {
+      registration.onupdatefound = function () {
+        if (navigator.serviceWorker.controller) {
+          const installingWorker = registration.installing;
+
+          installingWorker.onstatechange = function () {
+            switch (installingWorker.state) {
+              case 'installed':
+                console.log('updated');
+                break;
+              case 'redundant':
+                throw new Error('The installing service worker became redundant.');
+            }
+          };
+        }
+      };
+    }).catch(function (e) {
+      console.error('Error during service worker registration:', e);
+    });
+}
+
+
